f7647c69ee6089f2caa863c90edfc51f3d9852bb
Peter Hellberg <peter@c7.se>
2026-06-01 20:01:26 +0200

    cmd: No need to replace anything in spy.sh

--- main.go
+++ main.go
@@ -277,7 +277,7 @@ func replacer(cfg config, name string, data []byte) []byte {
 		data = replaceOne(data, "ff-author-id", cfg.authorID)
 
 		return data
-	case "README.md", "spy.sh":
+	case "README.md":
 		data = replaceOne(data, "ff-app-id", cfg.appID)
 		data = replaceOne(data, "ff-author-id", cfg.authorID)
 
